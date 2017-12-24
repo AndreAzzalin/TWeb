1f7d1c40d6749d4898ddca0e774bfb6f26080de5
Andrea <Andrea@fisso>
2017-12-24 16:42:40 +0100

    mvc init

---
+++ app/views/home/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrea
+ * Date: 24/12/2017
+ * Time: 15:04
+ */
+
+
+include ('C:\xampp\htdocs\TWeb\public\common\top.html');
+
+echo ini_get('include_path');
+
+?>
+
+
+
+<p> WELCOME TO INDEX </p>
+
+<?=
+$data['name']?>
