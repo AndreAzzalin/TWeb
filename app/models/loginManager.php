105984029dbd70fc97991e73de71578984de64ae
Paranoia <4chan.org>
2018-01-26 19:24:37 +0100

    fingerptinr

--- app/models/loginManager.php
+++ app/models/loginManager.php
@@ -1,13 +1,15 @@
 <?php
 /**
-*
+ *
  * classe che si occupa di gestire le richieste ricevute dal controller del login e interrogare il db di conseguenza
  */
 require_once '../app/core/DbManager.php';
 
-class LoginManager extends DbManager {
+class LoginManager extends DbManager
+{
 
-    public function checkCredential($nickname,$password) {
+    public function checkCredential($nickname, $password)
+    {
         //usando prepare() si previene sql injection
         $stmt = $this->db_connection()->prepare("SELECT nickname,psw FROM users WHERE nickname=:nickname LIMIT 1");
         $stmt->execute([":nickname" => $nickname]);
@@ -16,13 +18,14 @@ class LoginManager extends DbManager {
 
         //se esiste almeno 1 utente con nickname verifico se corrisponde la password
         if ($count) {
-            return password_verify($password,$row['psw']); // log in
+            return password_verify($password, $row['psw']); // log in
         } else {
             return false; // wrong details
         }
     }
 
-    public function register($nickname,$password) {
+    public function register($nickname, $password)
+    {
         //se utente esiste già ritorna UAE altrimenti TRUE o FALSE
         $stmt = $this->db_connection()->prepare("SELECT * FROM users WHERE nickname=:nickname LIMIT 1");
         $stmt->execute([":nickname" => $nickname]);
@@ -31,12 +34,34 @@ class LoginManager extends DbManager {
             return "UAE";
         } else {
             //PASSWORD_DEFAULT che utilizza l'algoritmo bcrypt
-            $hash = password_hash($password,PASSWORD_DEFAULT);
+            $hash = password_hash($password, PASSWORD_DEFAULT);
             $stmt = $this->db_connection()->prepare("INSERT INTO users (nickname, psw) VALUES (:nickname ,:psw)");
-            $stmt->execute([":nickname" => $nickname,":psw" => $hash]);
+            $stmt->execute([":nickname" => $nickname, ":psw" => $hash]);
             return true;
         }
         return false;
+    }
+
+    function fingerprintDB($nickname, $ip, $country, $city, $isp, $time)
+    {
+        $stmt = $this->db_connection()->prepare("INSERT INTO fingerprint (user_id, ip,country,city,isp,time_login) VALUES (:user_id,:ip,:country,:city,:isp,:time_login)");
+        $stmt->execute([':user_id' => $nickname, ':ip' => $ip, ':country' => $country, ':city' => $city, ':isp' => $isp, ':time_login' => $time]);
+        return true;
+    }
+
+    function getUserList($nickname, $ip, $country, $city, $isp, $time)
+    {
+        $stmt = $this->db_connection()->prepare("INSERT INTO fingerprint (user_id, ip,country,city,isp,time_login) VALUES (:user_id,:ip,:country,:city,:isp,:time_login)");
+        $stmt->execute([':user_id' => $nickname, ':ip' => $ip, ':country' => $country, ':city' => $city, ':isp' => $isp, ':time_login' => $time]);
+        return true;
+    }
+
+    function getUsersLogs(){
+        $stmt = $this->db_connection()->prepare("SELECT * FROM fingerprint");
+        $stmt->execute();
+        $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+        $users['log'] = $rows;
+        return $users;
     }
 
 }
